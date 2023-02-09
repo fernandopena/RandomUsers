0968972db9b3df7c4604c7ace23aca11129fcf5b
penafernando <fernando.pena@pomelo.la>
2023-02-09 15:19:36 -0300

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Fernando Pena on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
